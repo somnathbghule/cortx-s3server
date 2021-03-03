8f1d1ef8f5c884ca3335beceac93d4e9971bf119
Mohammed Shahid <mohammed.shahid@seagate.com>
2021-03-03 05:02:51 -0700

    Fixing ST's which was temporarily disabled.
    File modified :
    	st/clitests/s3cmd_spec.py
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>

--- st/clitests/s3cmd_spec.py
+++ st/clitests/s3cmd_spec.py
@@ -205,9 +205,9 @@ S3cmdTest('s3cmd can upload 3k file').upload_test("seagatebucket", "3kfile", 300
 
 S3cmdTest('s3cmd can download 3k file').download_test("seagatebucket", "3kfile").execute_test().command_is_successful().command_created_file("3kfile")
 
-#S3cmdTest('s3cmd can copy object 3k file').upload_copy_test("seagatebucket", "3kfile", "3kfile.copy").execute_test().command_is_successful()
+S3cmdTest('s3cmd can copy object 3k file').upload_copy_test("seagatebucket", "3kfile", "3kfile.copy").execute_test().command_is_successful()
 
-#S3cmdTest('s3cmd can move object 3k file').upload_move_test("seagatebucket", "3kfile", "seagatebucket", "3kfile.moved").execute_test().command_is_successful()
+S3cmdTest('s3cmd can move object 3k file').upload_move_test("seagatebucket", "3kfile", "seagatebucket", "3kfile.moved").execute_test().command_is_successful()
 
 # ************ FILE Overwrite TEST ************
 S3cmdTest('s3cmd can upload 3k file with size 3072').upload_test("seagatebucket", "3kfile", 3072).execute_test().command_is_successful()
@@ -240,7 +240,9 @@ S3cmdTest('s3cmd can list all objects').list_all_objects().execute_test().comman
 S3cmdTest('s3cmd can show disk usage').disk_usage_bucket("seagatebucket").execute_test().command_is_successful()
 
 # ************ DELETE OBJECT TEST ************
-S3cmdTest('s3cmd can delete 3k file').delete_test("seagatebucket", "3kfile").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete 3k file').delete_test("seagatebucket", "3kfile.moved").execute_test().command_is_successful()
+
+S3cmdTest('s3cmd can delete 3k file').delete_test("seagatebucket", "3kfile.copy").execute_test().command_is_successful()
 
 S3cmdTest('s3cmd should not have object after its delete').list_objects('seagatebucket').execute_test().command_is_successful().command_response_should_not_have('3kfile')
 
